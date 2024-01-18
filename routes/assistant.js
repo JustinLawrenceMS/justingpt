4d8a631f6f3e04b5995a13069c2fec328aea7bed
JustinLawrenceMS <justin@thechubbytech.com>
2024-01-17 18:30:43 -0600

    fix async handling of gpt reply

--- routes/assistant.js
+++ routes/assistant.js
@@ -5,24 +5,21 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-	const openai = new OpenAI();
+	const openai = new OpenAI({ apiKey: 'TODO: pull key from env' });
 
 	async function main() {
 	  const completion = await openai.chat.completions.create({
 	    messages: [{ role: "system", content: "You are a helpful assistant" }],
 	    model: "gpt-3.5-turbo",
 	  });
-
-	  console.log(completion.choices[0].message.content);
+		console.log(completion.choices[0].message.content);
+		return completion.choices[0].message.content;
 	}
-	main();
-	next();
-  }, (req, res, next) => {
-	console.log(res);
-	next();
-  }, (req, res) => {
-
-	  res.render('assistant', { message: res.toString() });
-  });
+	(async ()=>{
+		res.gpt = await main();
+		console.log(res.gpt);
+		res.render('assistant', { message: res.gpt });	
+	})();	    
+});
 
 module.exports = router;
