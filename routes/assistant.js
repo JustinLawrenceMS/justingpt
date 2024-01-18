7514b92ebbf0d77e6de306de725253a0726531b8
JustinLawrenceMS <justin@thechubbytech.com>
2024-01-17 19:42:26 -0600

    working backend

--- routes/assistant.js
+++ routes/assistant.js
@@ -5,20 +5,18 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-	const openai = new OpenAI({ apiKey: 'TODO: pull key from env' });
+	const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
 	async function main() {
 	  const completion = await openai.chat.completions.create({
-	    messages: [{ role: "system", content: "You are a helpful assistant" }],
+	    messages: [{ role: "system", content: req.query.gpt }],
 	    model: "gpt-3.5-turbo",
 	  });
-		console.log(completion.choices[0].message.content);
 		return completion.choices[0].message.content;
 	}
 	(async ()=>{
-		res.gpt = await main();
-		console.log(res.gpt);
-		res.render('assistant', { message: res.gpt });	
+		res.gptResponse = await main();
+		res.render('assistant', { message: res.gptResponse, title: process.env.APP_TITLE });	
 	})();	    
 });
 
